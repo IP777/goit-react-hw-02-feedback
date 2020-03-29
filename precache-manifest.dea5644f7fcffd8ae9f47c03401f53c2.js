c19c36ae9b26aee8749fc8a3fdb55df149a8bd01
IP777 <ivanov_piter@ukr.net>
2020-03-29 15:21:12 +0300

    Updates

---
+++ precache-manifest.dea5644f7fcffd8ae9f47c03401f53c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ad9d60e6a07c9cfd5386cd469f2701",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "5b602f853132f29ac24e",
+    "url": "/goit-react-hw-02-feedback/static/css/main.d4556488.chunk.css"
+  },
+  {
+    "revision": "7e86d4952ec14adddb52",
+    "url": "/goit-react-hw-02-feedback/static/js/2.4743bb42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.4743bb42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b602f853132f29ac24e",
+    "url": "/goit-react-hw-02-feedback/static/js/main.04635919.chunk.js"
+  },
+  {
+    "revision": "f59aa7dbc0213e80a103",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb39d68.js"
+  }
+]);
